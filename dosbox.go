85239dff56d0d70792c3cadf0ab98918dd39720a
Joshua Partogi <proscrumtrainer@live.com>
2015-01-18 15:19:59 +0800

    Restructure packages - prevent cyclomatic complexity

--- dosbox.go
+++ dosbox.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"github.com/jpartogi/dosboxgo/system"
+	"github.com/jpartogi/dosboxgo/command"
+	"github.com/jpartogi/dosboxgo/console"
+	"github.com/jpartogi/dosboxgo/filesystem"
 )
 
 func main() {
-	system.OrchestrateSystem()
+	drive := filesystem.NewDrive("C")
+	drive.Restore()
+
+	outputter := console.NewOutputter()
+
+	commands := command.Factory(drive, outputter)
+
+	invoker := command.NewInvoker(commands)
+
+	proc := command.NewProc(invoker, drive, outputter)
+	proc.ProcessInput()
 }
